28d58b1142d044edd713b0e93c9aa9f9c422ba24
Elena Zimnickaya <nazarova.leno4ka@yandex.ru>
2023-06-11 15:54:13 +0400

    Change appveyor

--- src/test/java/TestOrdingCardDelivery.java
+++ src/test/java/TestOrdingCardDelivery.java
@@ -1,11 +1,14 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class TestOrdingCardDelivery {
 
